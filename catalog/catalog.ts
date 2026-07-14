7ea09f1da9e59e59351b71916445e9d22c8e2f1f
Duarte <pedrohduarte007@gmail.com>
2026-07-14 11:01:56 -0300

    feat(catalog): Add registerProduct function

--- catalog/catalog.ts
+++ catalog/catalog.ts
@@ -1,32 +1,75 @@
-import promptSync from "prompt-sync"
-import { showOptionsCatalog, showProducts } from "../ui.js"
+import promptSync from "prompt-sync";
+import { showOptionsCatalog, showProducts } from "../ui.js";
+import { validationInputNumber,validationInputString } from "../validation/validation.js";
+import { createProduct,saveProduct } from "../products/product.js";
 
-const prompt = promptSync()
 
-let running:boolean = true
+const prompt = promptSync();
 
-function startCatalog():void{
-    while(running){
-        showOptionsCatalog()
-        let choice: number = Number(prompt("Qual número você deseja?"))
+let running: boolean = true;
+let input: string | number;
+let name: string;
+let price: number;
+let stock: number;
+let category: string;
 
-        switch(choice){
-            case 1:{
-                showProducts
-                break
-            }
-            case 2:{
+function startCatalog(): void {
+  while (running) {
+    showOptionsCatalog();
+    let choice: number = Number(prompt("Qual número você deseja?"));
 
-                break
-            }
+    switch (choice) {
+      case 1: {
+        showProducts;
+        break;
+      }
+      case 2: {
+        registerProduct()
+        break;
+      }
 
-            default:{
-                running = false
-                break
-            }
-        }
+      default: {
+        running = false;
+        break;
+      }
     }
+  }
 }
 
+function registerProduct(): void {
+  input = prompt("Qual é o nome do produto que você deseja adicionar? ");
+  if (!validationInputString(input)) {
+    console.log("Nome inválido, digite outro nome");
+    return;
+  }
+  name = input;
 
-export{startCatalog}
+  input = Number(
+    prompt("Qual é o preço do produto que você deseja adicionar? "),
+  );
+  if (!validationInputNumber(Number(input))) {
+    console.log("Preço inválido");
+    return;
+  }
+  price = input;
+
+  input = Number(
+    prompt("Qual é a quantidade do produto que você deseja adicionar? "),
+  );
+  if (!validationInputNumber(Number(input))) {
+    console.log("Quantidade inválida");
+    return;
+  }
+  stock = Number(input);
+
+  input = prompt("Qual é a categoria do produto que você deseja adicionar? ");
+  if (!validationInputString(input)) {
+    console.log("Nome inválido, digite outro nome");
+    return;
+  }
+  category = input;
+
+  saveProduct(createProduct(name, price, category, stock));
+}
+
+export { startCatalog };
